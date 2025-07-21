1f11f718189bed55a361ededae3f2d0b005bfad8
jagadw <jagadhitawicaksana@gmail.com>
2025-07-21 13:48:43 +0800

    hide some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.tsx';
-import QRScan from './pages/Qrscan.tsx';
-import Mission from './pages/Mission.tsx';
-import Upload from './pages/Upload.tsx';
+// import QRScan from './pages/Qrscan.tsx';
+// import Mission from './pages/Mission.tsx';
+// import Upload from './pages/Upload.tsx';
 import DashboardAdmin from './pages/DashboardAdmin.tsx';
 import DashboardUser from './pages/DashboardUser.tsx';
 
@@ -11,8 +11,8 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Login />} />
-        <Route path="/scan" element={<QRScan />} />
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/scan" element={<QRScan />} />
         <Route path="/mission" element={<Mission />} />
         <Route path="/upload" element={<Upload />} /> */}
         <Route path="/admin/*" element={<DashboardAdmin />} />
