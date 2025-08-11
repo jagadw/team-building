7cd3f5a30f3f358c14eb9de0a060219030812919
jagadw <jagadhitawicaksana@gmail.com>
2025-08-11 19:55:56 +0800

    fix login navigate

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,7 +29,7 @@ const Login: React.FC = () => {
       if (decoded.role === 'admin') {
         navigate('/adminmenu');
       } else if (decoded.role === 'participant') {
-        navigate('/qrscan');
+        navigate('/scan');
       } else {
         setError('Unknown role');
       }
