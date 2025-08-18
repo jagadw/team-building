d0883995c8833b7a484ac43d386ce5509672f844
totallynotisla <wkomangadi44@gmail.com>
2025-08-18 12:06:03 +0800

    add: disable eslint

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Upload from "./pages/Upload.tsx";
 import Menu from "./pages/Menu.tsx";
 import Leaderboard from "./pages/Leaderboard.tsx";
 import AdminMenu from "./pages/admin/AdminMenu.tsx";
+import ParticipantMenu from "./pages/admin/ParticipantMenu.tsx";
 import DashboardAdmin from "./pages/DashboardAdmin.tsx";
 import DashboardUser from "./pages/DashboardUser.tsx";
 import { Auth } from "./components/Auth.tsx";
@@ -20,9 +21,11 @@ const App: React.FC = () => {
 				<Route path="/admin/event/:slug/*" element={<DashboardAdmin />} />
 
 				<Route element={<Auth roles={["participant"]} />}>
+					<Route path="/participant" element={<ParticipantMenu />} />
 					<Route path="/event/:slug/*" element={<DashboardUser />} />
 					<Route path="/menu" element={<Menu />} />
 					<Route path="/leaderboard" element={<Leaderboard />} />
+					api
 					<Route path="/scan" element={<QRScan />} />
 					<Route path="/mission" element={<Mission />} />
 					<Route path="/upload" element={<Upload />} />
